f7df017d5fe8c4d459cd4d8e374ce3963ae1f263
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-07 00:23:54 +0200

    Dominosa: auto Seed, and show Game ID and Seed

--- dominosa.c
+++ dominosa.c
@@ -3430,6 +3430,7 @@ const struct game thegame = {
 };
 
 #ifdef STANDALONE_SOLVER
+#include <time.h>
 
 int main(int argc, char **argv)
 {
@@ -3438,13 +3439,21 @@ int main(int argc, char **argv)
     char *id = NULL, *desc;
     int maxdiff = DIFFCOUNT;
     const char *err;
-    bool grade = false, diagnostics = false;
+    bool grade = false, diagnostics = false, generate = false;
+    time_t seed = time(NULL);
     struct solver_scratch *sc;
     int retd;
 
     while (--argc > 0) {
         char *p = *++argv;
-        if (!strcmp(p, "-v")) {
+        if (!strcmp(p, "--seed")) {
+            if (argc == 0) {
+                fprintf(stderr, "--seed needs an argument");
+                return 1;
+            }
+            seed = (time_t) atoi(*++argv);
+            argc--;
+        } else if (!strcmp(p, "-v")) {
             diagnostics = true;
         } else if (!strcmp(p, "-g")) {
             grade = true;
@@ -3471,26 +3480,34 @@ int main(int argc, char **argv)
         }
     }
 
-    if (!id) {
-        fprintf(stderr, "usage: %s [-v | -g] <game_id>\n", argv[0]);
-        return 1;
-    }
-
-    desc = strchr(id, ':');
-    if (!desc) {
-        fprintf(stderr, "%s: game id expects a colon in it\n", argv[0]);
-        return 1;
-    }
-    *desc++ = '\0';
-
     p = default_params();
-    decode_params(p, id);
-    err = validate_desc(p, desc);
-    if (err) {
-        fprintf(stderr, "%s: %s\n", argv[0], err);
-        return 1;
+    if (id) {
+        decode_params(p, id);
+        desc = strchr(id, ':');
     }
+    if (id && desc) {
+        *desc++ = '\0';
+
+        err = validate_desc(p, desc);
+        if (err) {
+            fprintf(stderr, "%s: %s\n", argv[0], err);
+            return 1;
+        }
+    } else {
+        char *aux = NULL;
+        random_state *rs = random_new((void *) &seed, sizeof(time_t));
+        desc = new_game_desc(p, rs, &aux, false);
+        generate = true;
+    }
+
     s = new_game(NULL, p, desc);
+    if(generate) {
+        printf("Dominosa: %s\n", encode_params(p, true));
+        printf("Game ID: %s\n", desc);
+        printf("Seed: %ld\n", seed);
+        printf("%s\n", game_text_format(s));
+        return 0;
+    }
 
     solver_diagnostics = diagnostics;
     sc = solver_make_scratch(p->n);
