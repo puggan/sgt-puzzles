b335273d426a27e8ad94c308d99c3e9d5a822b8e
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-06 23:52:11 +0200

    Lightup: auto Seed, and show Game ID and Seed, no auto diagnostics

--- lightup.c
+++ lightup.c
@@ -57,10 +57,11 @@
  * true.
  */
 #if defined STANDALONE_SOLVER
-#define SOLVER_DIAGNOSTICS
-int verbose = 0;
+#ifdef SOLVER_DIAGNOSTICS
 #undef debug
 #define debug(x) printf x
+#endif
+int verbose = 0;
 #elif defined SOLVER_DIAGNOSTICS
 #define verbose 2
 #endif
@@ -2333,19 +2334,29 @@ const struct game thegame = {
 };
 
 #ifdef STANDALONE_SOLVER
+#include <time.h>
 
 int main(int argc, char **argv)
 {
     game_params *p;
     game_state *s;
-    char *id = NULL, *desc, *result;
+    char *id = NULL, *desc = NULL, *result;
     const char *err;
     int nsol, diff, really_verbose = 0;
     unsigned int sflags;
+    bool generate;
+    time_t seed = time(NULL);
 
     while (--argc > 0) {
         char *p = *++argv;
-        if (!strcmp(p, "-v")) {
+        if (!strcmp(p, "--seed")) {
+            if (argc == 0) {
+                fprintf(stderr, "--seed needs an argument");
+                return 1;
+            }
+            seed = (time_t) atoi(*++argv);
+            argc--;
+        } else if (!strcmp(p, "-v")) {
             really_verbose++;
         } else if (*p == '-') {
             fprintf(stderr, "%s: unrecognised option `%s'\n", argv[0], p);
@@ -2355,26 +2366,33 @@ int main(int argc, char **argv)
         }
     }
 
-    if (!id) {
-        fprintf(stderr, "usage: %s [-v] <game_id>\n", argv[0]);
-        return 1;
-    }
-
-    desc = strchr(id, ':');
-    if (!desc) {
-        fprintf(stderr, "%s: game id expects a colon in it\n", argv[0]);
-        return 1;
-    }
-    *desc++ = '\0';
-
     p = default_params();
-    decode_params(p, id);
-    err = validate_desc(p, desc);
-    if (err) {
-        fprintf(stderr, "%s: %s\n", argv[0], err);
-        return 1;
+    if (id) {
+        decode_params(p, id);
+        desc = strchr(id, ':');
+    }
+    if (id && desc) {
+        *desc++ = '\0';
+        err = validate_desc(p, desc);
+        if (err) {
+            fprintf(stderr, "%s: %s\n", argv[0], err);
+            return 1;
+        }
+    } else {
+        char *aux = NULL;
+        random_state *rs = random_new((void *) &seed, sizeof(time_t));
+        desc = new_game_desc(p, rs, &aux, false);
+        generate = true;
     }
     s = new_game(NULL, p, desc);
+
+    if(generate) {
+        printf("Towers: %s\n", encode_params(p, true));
+        printf("Game ID: %s\n", desc);
+        printf("Seed: %ld\n", seed);
+        printf("%s\n", game_text_format(s));
+        return 0;
+    }
 
     /* Run the solvers easiest to hardest until we find one that
      * can solve our puzzle. If it's soluble we know that the
